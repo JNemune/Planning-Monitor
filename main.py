33be3952b0018ecade05ed0b57074aa95749060e
JNemune <JNemune2002@gmail.com>
2023-02-13 06:02:24 -0500

    plot method plot percentage of time used and save the fig

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import datetime
 from json import dump, load
 from os.path import join
 
+import arabic_reshaper
+import matplotlib.pyplot as plt
+from bidi.algorithm import get_display
+
 
 class App(object):
     def __init__(self) -> None:
@@ -14,9 +18,55 @@ class App(object):
         pass
 
     def plot(self) -> None:
-        pass
+        """
+        plot percentage of time used and save the fig
+        """
 
-    def do(self, group: str, task: str, detail: str) -> bool:
+        def persian_text(inp: str):
+            return get_display(arabic_reshaper.reshape(inp))
+
+        with open(join(".", "target", "done.json"), "r", encoding="utf-8") as f:
+            done = load(f)
+        with open(join(".", "target", "tasks.json"), "r", encoding="utf-8") as f:
+            tasks = load(f)
+        fig, ax = plt.subplots()
+
+        bar_colors = [
+            "tab:blue",
+            "tab:orange",
+            "tab:green",
+            "tab:red",
+            "tab:purple",
+            "tab:brown",
+            "tab:pink",
+            "tab:gray",
+            "tab:olive",
+            "tab:cyan",
+        ]
+
+        for i_, i in enumerate(tasks):
+            for j_, j in enumerate(tasks[i]):
+                ax.bar(
+                    persian_text(j),
+                    100
+                    * (
+                        sum(k["time"] for k in done[i][j])
+                        if i in done and j in done[i]
+                        else 0
+                    )
+                    / tasks[i][j]
+                    / 3600,
+                    label=persian_text(i) if j_ == 0 else "_" + persian_text(i),
+                    color=bar_colors[i_],
+                )
+
+        ax.set_ylabel("Percentage of Time Used (%)")
+        ax.set_title("Planning Monitor")
+        ax.legend(title="Categories")
+
+        fig.savefig(join(".", "target", "plot.png"))
+
+    def do(self, category: str, task: str, detail: str) -> bool:
         """
         done tasks commit on target/done.json
         """
@@ -24,12 +74,12 @@ class App(object):
             done = load(f)
         with open(join(".", "target", "tasks.json"), "r", encoding="utf-8") as f:
             tasks = load(f)
-        if group not in tasks or task not in tasks[group]:
+        if category not in tasks or task not in tasks[category]:
             return False
         time = datetime.datetime.now() + self.CA_IR_delta
 
-        done[group] = done.get(group, {})
-        done[group][task] = done[group].get(task, []) + [
+        done[category] = done.get(category, {})
+        done[category][task] = done[category].get(task, []) + [
             {
                 "detail": detail,
                 "start_time": done["start_time"],
@@ -41,11 +91,14 @@ class App(object):
         ]
         done["start_time"] = time.isoformat()
 
-        with open(join(".", "target", "done.json"), "r", encoding="utf-8") as f:
+        with open(join(".", "target", "done.json"), "w", encoding="utf-8") as f:
             dump(done, f, ensure_ascii=False)
 
     def telegram_manager(self) -> None:
         pass
 
 
-print(datetime.datetime.now() + datetime.timedelta(hours=8, minutes=30))
+if __name__ == "__main__":
+    app = App()
+    app.do("متفرقه", "خواب", "درس")
+    app.plot()
